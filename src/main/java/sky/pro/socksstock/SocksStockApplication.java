bed6bcbf64f1160321a387a86439e75f87f62544
svetlanajJd <artlanago@gmail.com>
2023-04-08 16:21:47 +0300

    первый коммит

---
+++ src/main/java/sky/pro/socksstock/SocksStockApplication.java
@@ -0,0 +1,13 @@
+package main.java.sky.pro.socksstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksStockApplication.class, args);
+	}
+
+}
